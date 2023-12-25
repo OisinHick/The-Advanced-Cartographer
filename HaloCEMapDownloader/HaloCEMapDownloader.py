d81e1cdd9bc5db3970a3c32e0facbb2a354c067f
Oisin Hickey <39494199+ironic833@users.noreply.github.com>
2023-12-25 11:37:13 +0000

    Update HaloCEMapDownloader.py
    - Working scraper for pages. Need to increase the range in the loop to scrape every page

--- HaloCEMapDownloader/HaloCEMapDownloader.py
+++ HaloCEMapDownloader/HaloCEMapDownloader.py
@@ -4,37 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 URL = "http://hce.halomaps.org/index.cfm?PG=1"
-
-# Set up Chrome options for headless mode
-chrome_options = Options()
-chrome_options.add_argument('--headless')  # Run Chrome in headless mode
-
-# Provide the path to the ChromeDriver executable
-cService = webdriver.ChromeService(executable_path='D:\\Github\\Halo-CE-Map-Downloads-System\\HaloCEMapDownloader\\chromedriver.exe')
-browser = webdriver.Chrome(service=cService, options=chrome_options)
-
-browser.get(URL)
-browser.implicitly_wait(320)
-html = browser.page_source
-soup = BeautifulSoup(html, "html.parser")
-
-# Find an element with the class 'filelink'
-filelink_element = soup.find('a', class_='filelink')
-
-# Print the content of the found element
-print(filelink_element)
-
-browser.quit()
-
-
-
- """
-
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-URL = "http://hce.halomaps.org/index.cfm?PG=1"
+URLPageOne = "http://hce.halomaps.org/index.cfm?PG=1&sid=0&Start=21&sort=1&cd=0&fc=0&fc2=0"
 
 # Set up Chrome options for headless mode
 chrome_options = Options()
@@ -57,3 +27,43 @@ for filelink_element in filelink_elements:
     print(filelink_element)
 
 browser.quit()
+ """
+
+# http://hce.halomaps.org/index.cfm?PG=1&sid=0&Start=61&sort=1&cd=0&fc=0&fc2=0
+# http://hce.halomaps.org/index.cfm?PG=1&sid=0&Start=81&sort=1&cd=0&fc=0&fc2=0
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def scrape_filelinks(url):
+    # Set up Chrome options for headless mode
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')  # Run Chrome in headless mode
+
+    # Provide the path to the ChromeDriver executable
+    cService = webdriver.ChromeService(executable_path='D:\\Github\\Halo-CE-Map-Downloads-System\\HaloCEMapDownloader\\chromedriver.exe')
+    browser = webdriver.Chrome(service=cService, options=chrome_options)
+
+    browser.get(url)
+    browser.implicitly_wait(320)
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Find all elements with the class 'filelink'
+    filelink_elements = soup.find_all('a', class_='filelink')
+
+    # Print the content of each found element
+    for filelink_element in filelink_elements:
+        print(filelink_element)
+
+    browser.quit()
+
+# Run the scraping function for the first URL
+scrape_filelinks("http://hce.halomaps.org/index.cfm?PG=1")
+
+# Run the scraping function for the second URL with Start parameter incremented by 20
+for i in range(0, 10):  # Adjust the range based on how many times you want to increment Start
+    start_value = 21 + i * 20
+    url_page_one = f"http://hce.halomaps.org/index.cfm?PG=1&sid=0&Start={start_value}&sort=1&cd=0&fc=0&fc2=0"
+    scrape_filelinks(url_page_one)
