bed24f845eefe95ed09088e19a1651c0976a907a
Oisin Hickey <39494199+ironic833@users.noreply.github.com>
2024-01-10 02:37:36 +0000

    Updates
    - Working version of the code base which scrapes each page and then downloads each map on the multiplayer section

--- HaloCEMapDownloader/HaloCEMapDownloader.py
+++ HaloCEMapDownloader/HaloCEMapDownloader.py
@@ -1,18 +1,14 @@
-
+import os
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from urllib.parse import urljoin
 
-DownloadUrl = "http://hce.halomaps.org/index.cfm?fid=7129"
-
-# Scrapes the URLs for the page and formatting to be just the links
+# Function to scrape file links
 def scrape_filelinks(url):
-    # Set up Chrome options for headless mode
     chrome_options = Options()
-    chrome_options.add_argument('--headless')  # Run Chrome in headless mode
-
-    # Provide the path to the ChromeDriver executable
+    chrome_options.add_argument('--headless')
     cService = webdriver.ChromeService(executable_path='D:\\Github\\Halo-CE-Map-Downloads-System\\HaloCEMapDownloader\\chromedriver.exe')
     browser = webdriver.Chrome(service=cService, options=chrome_options)
 
@@ -21,28 +17,21 @@ def scrape_filelinks(url):
     html = browser.page_source
     soup = BeautifulSoup(html, "html.parser")
 
-    # Find all elements with the class 'filelink'
-    filelink_elements = soup.find_all('a', class_='filelink')
-
-    # Extract href attribute and create complete URL
+    filelink_elements = soup.find_all('a', class_='image-fade')
     for filelink_element in filelink_elements:
         href_value = filelink_element.get('href')
         if href_value:
-            complete_url = urljoin("http://hce.halomaps.org/", href_value)
+            complete_url = urljoin("https://www.halomaps.org/hce/", href_value)
             print(complete_url)
             scrape_filelinks_download(complete_url)
 
     browser.quit()
 
-# Scrapes the URLs for the page and formatting to be just the links
+# Function to scrape file links and send a POST request
 def scrape_filelinks_download(url):
-
-    # Set up Chrome options for headless mode
     chrome_options = Options()
-    chrome_options.add_argument('--headless')  # Run Chrome in headless mode
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument(f'--download.default_directory=D:\Github\Halo-CE-Map-Downloads-System\HaloCEMapDownloader\downloads')
-
-    # Provide the path to the ChromeDriver executable
     cService = webdriver.ChromeService(executable_path='D:\\Github\\Halo-CE-Map-Downloads-System\\HaloCEMapDownloader\\chromedriver.exe')
     browser = webdriver.Chrome(service=cService, options=chrome_options)
 
@@ -51,20 +40,52 @@ def scrape_filelinks_download(url):
     html = browser.page_source
     soup = BeautifulSoup(html, "html.parser")
 
-    # Find the form element with the specified attributes
-    form_element = soup.find('form', {'action': 'index.cfm', 'method': 'post'})
+    # Extracting values
+    fid = soup.find('input', {'name': 'fid'})['value']
+    action = soup.find('input', {'name': 'action'})['value']
+    hash_value = soup.find('input', {'name': 'hash'})['value']
 
-    # Print the HTML source of the found form element
-    if form_element:
-        print(form_element.prettify())
+    if fid and action and hash_value:
+        print("Found the target form:")
+        print(fid, action, hash_value)
+
+        # Sending POST request
+        post_data = {
+            'fid': fid,
+            'action': action,
+            'hash': hash_value
+        }
+        post_url = 'https://www.halomaps.org/hce/detail.cfm'
+        response = requests.post(post_url, data=post_data)
+
+        # Handle the response as needed
+        if response.status_code == 200:
+            # Extracting the filename from the content-disposition header, if available
+            content_disposition = response.headers.get('content-disposition')
+            if content_disposition:
+                filename = content_disposition.split("filename=")[-1]
+            else:
+                filename = "downloaded_file.zip"
+
+            # Save the response content to a file
+            file_path = os.path.join('D:\Github\Halo-CE-Map-Downloads-System\HaloCEMapDownloader\downloads', filename)
+            with open(file_path, 'wb') as file:
+                file.write(response.content)
+            
+            print(f"Downloaded file: {file_path}")
+        else:
+            print(f"Failed to download file. Status code: {response.status_code}")
+
+    else:
+        print("Target form not found.")
 
     browser.quit()
 
 # Run the scraping function for the first URL
-scrape_filelinks("http://hce.halomaps.org/index.cfm?PG=1")
+scrape_filelinks("https://www.halomaps.org/hce/index.cfm?sid=10")
 
-# Run the scraping function for the second URL with Start parameter incremented by 20
+# Run the scraping function for the second URL with Start parameter incremented by 30
 for i in range(0, 400):  # Adjust the range based on how many times you want to increment Start
-    start_value = 21 + i * 20
-    url_page_one = f"http://hce.halomaps.org/index.cfm?PG=1&sid=0&Start={start_value}&sort=1&cd=0&fc=0&fc2=0"
+    start_value = 31 + i * 30
+    url_page_one = f"https://www.halomaps.org/hce/index.cfm?sid=10&sort=1&Start={start_value}"
     scrape_filelinks(url_page_one)
